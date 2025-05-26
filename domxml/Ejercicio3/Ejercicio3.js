06cf7fa35e1b862dbf74215510ae5d798c3c0138
Hugopdam <hugopdam@gmail.com>
2025-05-26 21:13:57 +0200

    Add files via upload

--- domxml/Ejercicio3/Ejercicio3.js
+++ domxml/Ejercicio3/Ejercicio3.js
@@ -1,77 +1,73 @@
 let xmlDoc = null;
 
-// Cargar el XML al iniciar
-window.onload = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "books.xml", true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      xmlDoc = xhr.responseXML;
+window.onload = () => {
+  const req = new XMLHttpRequest();
+  req.open("GET", "books.xml", true);
+  req.onreadystatechange = () => {
+    if (req.readyState === 4 && req.status === 200) {
+      xmlDoc = req.responseXML;
     }
   };
-  xhr.send();
+  req.send();
 };
 
-function mostrarTitulo1() {
+function verPrimerTitulo() {
   const titulo = xmlDoc.getElementsByTagName("book")[0]
-                .getElementsByTagName("title")[0].textContent;
-  mostrarResultado(`<strong>Título del primer libro:</strong> ${titulo}`);
+                       .getElementsByTagName("title")[0].textContent;
+  mostrar(`<strong>Primer título:</strong> ${titulo}`);
 }
 
-function mostrarTodosTitulos() {
-  const books = xmlDoc.getElementsByTagName("book");
-  let titulos = "";
-  for (let i = 0; i < books.length; i++) {
-    titulos += books[i].getElementsByTagName("title")[0].textContent + "<br>";
-  }
-  mostrarResultado(`<strong>Todos los títulos:</strong><br>${titulos}`);
-}
-
-function mostrarNumAtributosLibro4() {
-  const book4 = xmlDoc.getElementsByTagName("book")[3];
-  const numAttrs = book4.attributes.length;
-  mostrarResultado(`<strong>Número de atributos del cuarto libro:</strong> ${numAttrs}`);
-}
-
-function mostrarValoresAtributosLibro4() {
-  const book4 = xmlDoc.getElementsByTagName("book")[3];
-  let valores = "";
-  for (let i = 0; i < book4.attributes.length; i++) {
-    valores += `${book4.attributes[i].name}: ${book4.attributes[i].value}<br>`;
-  }
-  mostrarResultado(`<strong>Valores de los atributos del cuarto libro:</strong><br>${valores}`);
-}
-
-function mostrarNumAutoresLibro3() {
-  const autores = xmlDoc.getElementsByTagName("book")[2]
-                 .getElementsByTagName("author");
-  mostrarResultado(`<strong>Número de autores del tercer libro:</strong> ${autores.length}`);
-}
-
-function mostrarAutoresLibro3() {
-  const autores = xmlDoc.getElementsByTagName("book")[2]
-                 .getElementsByTagName("author");
+function verTodosTitulos() {
+  const libros = xmlDoc.getElementsByTagName("book");
   let lista = "";
-  for (let i = 0; i < autores.length; i++) {
-    lista += autores[i].textContent + "<br>";
+  for (let libro of libros) {
+    lista += libro.getElementsByTagName("title")[0].textContent + "<br>";
   }
-  mostrarResultado(`<strong>Autores del tercer libro:</strong><br>${lista}`);
+  mostrar(`<strong>Todos los títulos:</strong><br>${lista}`);
 }
 
-function mostrarTabla() {
-  const books = xmlDoc.getElementsByTagName("book");
-  let tabla = `<table><tr><th>Título</th><th>Primer autor</th><th>Precio</th><th>Año</th></tr>`;
-  for (let i = 0; i < books.length; i++) {
-    const title = books[i].getElementsByTagName("title")[0]?.textContent || "N/A";
-    const author = books[i].getElementsByTagName("author")[0]?.textContent || "N/A";
-    const price = books[i].getElementsByTagName("price")[0]?.textContent || "N/A";
-    const year = books[i].getElementsByTagName("year")[0]?.textContent || "N/A";
-    tabla += `<tr><td>${title}</td><td>${author}</td><td>${price}</td><td>${year}</td></tr>`;
+function atributosLibro() {
+  const libro = xmlDoc.getElementsByTagName("book")[3];
+  mostrar(`<strong>Atributos del cuarto libro:</strong> ${libro.attributes.length}`);
+}
+
+function valoresAtributos() {
+  const libro = xmlDoc.getElementsByTagName("book")[3];
+  let datos = "";
+  for (let i = 0; i < libro.attributes.length; i++) {
+    datos += `${libro.attributes[i].name}: ${libro.attributes[i].value}<br>`;
+  }
+  mostrar(`<strong>Valores de los atributos:</strong><br>${datos}`);
+}
+
+function cuentaAutores() {
+  const autores = xmlDoc.getElementsByTagName("book")[2].getElementsByTagName("author");
+  mostrar(`<strong>Número de autores del tercer libro:</strong> ${autores.length}`);
+}
+
+function listarAutores() {
+  const autores = xmlDoc.getElementsByTagName("book")[2].getElementsByTagName("author");
+  let nombres = "";
+  for (let autor of autores) {
+    nombres += autor.textContent + "<br>";
+  }
+  mostrar(`<strong>Autores:</strong><br>${nombres}`);
+}
+
+function verTabla() {
+  const libros = xmlDoc.getElementsByTagName("book");
+  let tabla = `<table><tr><th>Título</th><th>Autor principal</th><th>Precio</th><th>Año</th></tr>`;
+  for (let libro of libros) {
+    const t = libro.getElementsByTagName("title")[0]?.textContent || "N/A";
+    const a = libro.getElementsByTagName("author")[0]?.textContent || "N/A";
+    const p = libro.getElementsByTagName("price")[0]?.textContent || "N/A";
+    const y = libro.getElementsByTagName("year")[0]?.textContent || "N/A";
+    tabla += `<tr><td>${t}</td><td>${a}</td><td>${p}</td><td>${y}</td></tr>`;
   }
   tabla += `</table>`;
-  mostrarResultado(`<strong>Tabla de libros:</strong><br>${tabla}`);
+  mostrar(`<strong>Libros disponibles:</strong><br>${tabla}`);
 }
 
-function mostrarResultado(html) {
-  document.getElementById("output").innerHTML = html;
+function mostrar(html) {
+  document.getElementById("resultado").innerHTML = html;
 }
